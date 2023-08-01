afc8f6f971ceae143868395f544473b46d3ee7fa
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-02 00:31:50 +0200

    converted indentation to tabs

--- project/app.py
+++ project/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 # Route for the home page
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+	return 'Hello, World!'
 
 if __name__ == '__main__':
-    # Run the application on localhost and port 5000
-    app.run(host='0.0.0.0', port=5000)
+	# Run the application on localhost and port 5000
+	app.run(host='0.0.0.0', port=5000)
