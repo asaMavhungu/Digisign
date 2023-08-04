f19347c960c5d9aa03134c6da56188196385035a
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-04 10:15:20 +0200

    Added home template

--- project/app.py
+++ project/app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Create the Flask application
 app = Flask(__name__)
 
 # Route for the home page
 @app.route('/')
-def hello():
+def hello() -> str:
 	return 'Hello, World!'
+
+@app.route("/home")
+def home() -> str:
+	return render_template("index.html")
+
 
 if __name__ == '__main__':
 	# Run the application on localhost and port 5000
