ae2132cb60bc14b6902518085d23d95c59954143
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-09 03:09:55 +0200

    Added links

--- project/static/script.js
+++ project/static/script.js
@@ -7,5 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add the 'carousel-fade' class to enable the crossfade effect
     wrap: true // Prevent wrapping at the end of slides
   });
-  // TODO: Redo slideshow
+  // TODO: Redo slideshow 
+  // https://www.youtube.com/watch?v=ku_97a6Bgkg
+  // https://www.youtube.com/watch?v=dam0GPOAvVI&t=3795s
 });
