4c989bec60715ac5cf7c99da10034ba3a52c4dc8
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-10 15:59:20 +0200

    Added sign-up forms logic

--- project/auth.py
+++ project/auth.py
@@ -1,15 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
+from typing import Optional
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-def home():
-	return "<h1>Login</h1>"
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+	data = request.form
+	print(data)
+	return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
 	return "<h1>logged out</h1>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-	return "<h1>Sign Up</h1>"
+	if request.method == 'POST':
+		email: Optional[str] = request.form.get('email')
+		firstName: Optional[str] = request.form.get('firstName')
+		password1: Optional[str] = request.form.get('password1')
+		password2: Optional[str] = request.form.get('password2')
+
+		if len(email) < 4: # type: ignore
+			flash("Email must be greater than 3 characters", category='error')
+		elif len(firstName) < 2: # type: ignore
+			flash("Firstname must be greater than 2 characters", category='error')
+		elif password1 != password2: # type: ignore
+			flash("Passwords don\'t match", category='error')
+		elif len(password1) < 7: # type: ignore
+			flash("Password must be greater than 6 characters", category='error')
+		else:
+			flash("Account created", category='success')
+			pass
+	data = request.form
+	print(data)
+	return render_template("sign_up.html")
