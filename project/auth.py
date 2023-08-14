838fe1ca00618d8afa9aa2649cac11aa7d3af4d8
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-14 16:22:24 +0200

    added signup logic

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Optional
 
 auth = Blueprint('auth', __name__)
@@ -30,8 +33,12 @@ def sign_up():
 		elif len(password1) < 7: # type: ignore
 			flash("Password must be greater than 6 characters", category='error')
 		else:
+			new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256')) # type: ignore
+			db.session.add(new_user)
+			db.session.commit()
 			flash("Account created", category='success')
-			pass
+			return redirect(url_for('views.home'))
+
 	data = request.form
 	print(data)
 	return render_template("sign_up.html")
