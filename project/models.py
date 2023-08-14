8b1773f0f7df2753c636ceabe501483bcc32c938
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-14 16:20:58 +0200

    added database models

--- project/models.py
+++ project/models.py
@@ -2,7 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Node(db.Model):
+class Note(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	data = db.Column(db.String(150))
 	date = db.Column(db.DateTime(timezone=True), default=func.now())
