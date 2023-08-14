84fedeeb5f8aeeecfafd44479f392473ff329c2c
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-14 16:20:38 +0200

    added database logiv

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -15,5 +16,16 @@ def create_app():
 
 	app.register_blueprint(views, url_prefix='/')
 	app.register_blueprint(auth, url_prefix='/')
+
+	from .models import User, Note
+
+	create_database(app)
 	
 	return app
+
+def create_database(app):
+	if not path.exists('project' + DB_NAME):
+		with app.app_context():
+			db.create_all()
+		print("Database created")
+		
