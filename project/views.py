28f2744f837bdd56db1f920da24e29de6a23f972
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-17 15:22:14 +0200

    added more classes

--- project/views.py
+++ project/views.py
@@ -1,10 +1,21 @@
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Slide
+from .models import Slide, VideoSlide
 from . import db
 import json
 
 views = Blueprint('views', __name__)
+
+def get_time_interval(slide):
+	# Present the slide for the calculated time interval
+	if isinstance(slide, VideoSlide):
+		time_interval = calculate_video_length(slide.video_url)
+	else:
+		time_interval = slide.time_interval
+
+def calculate_video_length(video_url):
+	#TODO add logic to get video length from url
+	return 120 
 
 
 @views.route('/')
@@ -26,6 +37,11 @@ def slides():
 			flash('Slide added', category='success')
 	return render_template("slideshow.html", user=current_user)
 
+@views.route('/slides', methods=['GET', 'POST'])
+@login_required
+def edit_slide():
+	#TODO Add functionality to edit slides
+	return 'Edit page'
 
 @views.route('/delete-slide', methods=['POST'])
 @login_required
