e651ca559f6c0d046fcf8e5fb7e4fa179b2000f2
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-08-17 03:24:18 +0200

    configured tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,74 @@
-# test_app.py
 import pytest
-from flask.testing import FlaskClient
-from project.app import app  # Import the Flask app instance from app.py
+from flask import url_for
 
-# Define a pytest fixture called "client"
+import sys
+print(sys.path)
+
+from project import create_app, db
+from project.models import User
+
+
 @pytest.fixture
-def client():
-	app.config['TESTING'] = True  # Set TESTING configuration to True to enable testing mode
-	client: FlaskClient = app.test_client()    # Create a test client for the Flask app
-	yield client                  # Yield the test client to the test function
-								 # (code below 'yield' will be executed after the test function)
+def app():
+	app = create_app()
+	app.config['TESTING'] = True
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+	with app.app_context():
+		db.create_all()
+		yield app
+		db.drop_all() # Clear database after test
 
-# Define a test function called "test_hello"
-def test_hello(client):
-	response = client.get('/')  # Send a GET request to the root ('/') route using the test client
-	assert response.status_code == 200  # Assert that the response status code is 200 (OK)
-	assert b'Hello, World!' in response.data  # Assert that the response contains 'Hello, World!'
+@pytest.fixture
+def client(app):
+	return app.test_client()
+
+def test_home_page(client):
+	response = client.get('/')
+	assert response.status_code == 200
+	assert b'Home2' in response.data
+
+def test_login_page(client):
+	response = client.get('/login')
+	assert response.status_code == 200
+	assert b'Login' in response.data
+
+def test_signup_and_login(app, client):
+	with app.test_request_context():
+		# Simulate signing up a user
+		signup_data = {
+			'email': 'testuser@example.com',
+			'firstName': 'Test',
+			'password1': 'testpassword',
+			'password2': 'testpassword',
+		}
+		signup_response = client.post('/sign-up', data=signup_data, follow_redirects=True)
+		assert signup_response.status_code == 200  # Assuming successful signup returns a 200 status code
+
+		# Simulate logging in with the newly signed-up user
+		login_response = client.post('/login', data={'email': signup_data['email'], 'password': signup_data['password1']})
+		assert login_response.status_code == 302  # Redirect after successful login
+		assert login_response.headers['Location'] == url_for('views.home')
+
+def test__invalid_password_login(app, client):
+	with app.test_request_context():
+		# Simulate signing up a user
+		signup_data = {
+			'email': 'testuser@example.com',
+			'firstName': 'Test',
+			'password1': 'testpassword',
+			'password2': 'testpassword',
+		}
+		signup_response = client.post('/sign-up', data=signup_data, follow_redirects=True)
+		assert signup_response.status_code == 200  # Assuming successful signup returns a 200 status code
+
+		# Simulate logging in with the newly signed-up user
+		login_response = client.post('/login', data={'email': signup_data['email'], 'password': 'wrongpassword'})
+		assert login_response.status_code == 200  # No redirect, wrong password
+
+
+
+def test_invalid_email_login(client):
+	response = client.post('/login', data={'email': 'testuser@example.com', 'password': 'wrongpassword'})
+	assert response.status_code == 200
+	assert b'Email does not exist.' in response.data
+    
