527c9b7a9fe727babe66af75455e48019c971113
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-09-08 03:14:09 +0200

    Registed imageSlide component

--- digisignFrontend/src/main.js
+++ digisignFrontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ImageSlide from './components/ImageSlide.vue' // Import your ImageSlide component
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// Register your ImageSlide component globally
+app.component('ImageSlide', ImageSlide)
+
+app.mount('#app')
