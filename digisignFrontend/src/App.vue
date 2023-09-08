4ed1b74c08b50a279b30ba9ed58c26f1230f7de5
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-09-08 03:13:44 +0200

    Made barebones slideshow woth images

--- digisignFrontend/src/App.vue
+++ digisignFrontend/src/App.vue
@@ -1,18 +1,3 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
 
 <style scoped>
 .logo {
@@ -28,3 +13,43 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+
+<!-- App.vue -->
+<template>
+  <div id="app">
+    <Slideshow :slides="slides"></Slideshow>
+  </div>
+</template>
+
+<script>
+import Slideshow from "./components/Slideshow.vue";
+import ImageSlide from "./components/ImageSlide.vue";
+
+export default {
+  components: {
+    Slideshow,
+    ImageSlide,
+  },
+  data() {
+    return {
+      slides: [
+        {
+          component: "ImageSlide",
+          data: {
+            imageUrl: "https://rare-gallery.com/thumbnail/1375167-luffy-sun-god-nika-gear-5-one-piece-4k-pc-wallpaper.jpg",
+          },
+        },
+        {
+          component: "ImageSlide",
+          data: {
+            imageUrl: "https://www.dexerto.com/cdn-cgi/image/width=3840,quality=75,format=auto/https://editors.dexerto.com/wp-content/uploads/2023/08/13/one-piece-gear-5.jpeg",
+          },
+        },
+        // Add more slides as needed
+      ],
+    };
+  },
+};
+</script>
+
