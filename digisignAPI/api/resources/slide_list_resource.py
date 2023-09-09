a88544d2e1616c5c9fa3af787a551932ced7f5ef
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-09-09 06:06:58 +0200

    slde_resources.py: removed marshal decorator on POST, added content_type attr and DELETE

--- digisignAPI/api/resources/slide_list_resource.py
+++ digisignAPI/api/resources/slide_list_resource.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, jsonify
 from flask_restful import Resource, reqparse, marshal_with, fields
 from bson.objectid import ObjectId
 from api.models.Slide import Slide
@@ -8,6 +8,7 @@ from api.models.Department import Department
 slide_parser = reqparse.RequestParser()
 slide_parser.add_argument('title', type=str, required=True, help='Title of the slide')
 slide_parser.add_argument('content', type=str, required=True, help='Content of the slide')
+slide_parser.add_argument('content_type', type=str, required=True, help='Type of content of the slide') 
 slide_parser.add_argument('author_id', type=str, required=True, help='Author ID of the slide')
 slide_parser.add_argument('departments', type=list, location='json', help='Departments associated with the slide')
 
@@ -16,6 +17,7 @@ slide_fields = {
 	'_id': fields.String(attribute='_id'),
 	'title': fields.String,
 	'content': fields.String,
+	'content_type': fields.String,
 	'author_id': fields.String,
 	'departments': fields.List(fields.String),
 }
@@ -38,7 +40,6 @@ class SlideList(Resource):
 		slides = [Slide.from_dict(slide_data) for slide_data in slides_data]
 		return slides, 200
 
-	@marshal_with(slide_fields)
 	def post(self):
 		"""
 		Create a new slide.
@@ -46,13 +47,14 @@ class SlideList(Resource):
 		args = slide_parser.parse_args()
 		title = args['title']
 		content = args['content']
+		content_type = args['content_type']
 		author_id = args['author_id']
 		departments = args.get('departments', [])
 
 		if Slide.find_by_title(title, self.mongo):
 			return {"message": f"Slide titled [{title}] already exists"}, 400
 
-		slide = Slide(title, content, author_id)
+		slide = Slide(title, content, content_type, author_id)
 
 		for department_name in departments:
 			department = Department.find_by_name(department_name, self.mongo)
@@ -67,25 +69,25 @@ class SlideList(Resource):
 		return {'message': 'Slide created', 'slide_id': slide_id}, 201
 	
 	def delete(self):
-			"""
-			Delete multiple slides.
+		"""
+		Delete multiple slides.
 
-			Returns:
-				dict: A message indicating the result of the deletion.
-			"""
-			data = request.get_json()
-			if not data:
-				return {"message": "No data provided in the request body"}, 400
+		Returns:
+			dict: A message indicating the result of the deletion.
+		"""
+		data = request.get_json()
+		if not data:
+			return {"message": "No data provided in the request body"}, 400
 
-			slide_titles = data.get("slide_titles", [])
-			if not slide_titles:
-				return {"message": "No slide titles provided in the request"}, 400
+		slide_titles = data.get("slide_titles", [])
+		if not slide_titles:
+			return {"message": "No slide titles provided in the request"}, 400
 
-			deleted_count = 0
-			for slide_title in slide_titles:
-				slide = Slide.find_by_title(slide_title, self.mongo)
-				if slide:
-					self.mongo.db.slides.delete_one({'_id': slide._id})
-					deleted_count += 1
+		deleted_count = 0
+		for slide_title in slide_titles:
+			slide = Slide.find_by_title(slide_title, self.mongo)
+			if slide:
+				self.mongo.db.slides.delete_one({'_id': slide._id})
+				deleted_count += 1
 
-			return {"message": f"{deleted_count} slides deleted"}, 200
+		return {"message": f"{deleted_count} slides deleted"}, 200
