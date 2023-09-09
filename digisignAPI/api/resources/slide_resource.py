dcd2f0ffe2ea43e4ab7bd67adf47af3ad08b3ac6
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-09-09 04:33:39 +0200

    refactored to use marshal_with

--- digisignAPI/api/resources/slide_resource.py
+++ digisignAPI/api/resources/slide_resource.py
@@ -1,13 +1,8 @@
 from flask import request
-from flask_restful import Resource, reqparse
+from flask_restful import Resource, reqparse, marshal_with, fields
 from bson.objectid import ObjectId
 from api.models.Slide import Slide
 from api.models.Department import Department
-
-
-# Define request parsers
-#department_parser = reqparse.RequestParser()
-#department_parser.add_argument('name', type=str, required=True, help='Name of the department')
 
 slide_parser = reqparse.RequestParser()
 slide_parser.add_argument('title', type=str, required=True, help='Title of the slide')
@@ -21,30 +16,32 @@ slide_parser_patch.add_argument('content', type=str, required=False, help='Conte
 slide_parser_patch.add_argument('author_id', type=str, required=False, help='Author ID of the slide')
 slide_parser_patch.add_argument('departments', type=list, location='json', help='Departments associated with the slide')
 
+slide_fields = {
+	'_id': fields.String(attribute='_id'),
+	'title': fields.String,
+	'content': fields.String,
+	'author_id': fields.String,
+	'departments': fields.List(fields.String),
+}
 
 class SlideResource(Resource):
-
 	def __init__(self, mongo):
 		self.mongo = mongo
 
+	@marshal_with(slide_fields)
 	def get(self, slide_title):
 		slide = Slide.find_by_title(slide_title, self.mongo)
-		#print(type(slide))
-		#user = User.from_dict(userDict)
 		if slide:
-			print(slide.to_dict())
-			return slide.to_dict(), 200
-			#return user.to_dict()
+			return slide, 200
 		return {"message": "Slide not found"}, 404
 
+	@marshal_with(slide_fields)
 	def post(self):
 		args = slide_parser.parse_args()
 		title = args['title']
 		content = args['content']
 		author_id = args['author_id']
 		departments = args.get('departments', [])
-		
-		# Create a new slide instance
 
 		if Slide.find_by_title(title, self.mongo):
 			return {"message": f"Slide titled [{title}] already exists"}, 400
@@ -55,20 +52,17 @@ class SlideResource(Resource):
 			department = Department.find_by_name(department_name, self.mongo)
 
 			if department:
-				# Associate the department with the slide
 				slide.add_department(department.name)
 			else:
 				return {"message": f"Department [{department_name}] not found"}, 404
 
-		# Save the slide to the database
 		slide_id = slide.save(self.mongo)
 
 		return {'message': 'Slide created', 'slide_id': slide_id}, 201
 
+	@marshal_with(slide_fields)
 	def patch(self, slide_title):
-		args = slide_parser_patch.parse_args()	
-
-		# Find the slide by slide_id
+		args = slide_parser_patch.parse_args()
 		slide = Slide.find_by_title(slide_title, self.mongo)
 
 		if not slide:
@@ -77,21 +71,18 @@ class SlideResource(Resource):
 		if 'departments' in args:
 			args = slide_parser.parse_args()
 			new_departments = args.get('departments', [])
-			print(new_departments)
-			# Ensure that new_departments is a list
+
 			if not isinstance(new_departments, list):
 				return {"message": "Invalid 'departments' format, expected a list"}, 400
-			# Create or retrieve the department (you should have a Department class)
-			
+
 			for department_name in new_departments:
 				department = Department.find_by_name(department_name, self.mongo)
 
 				if department:
-					# Associate the department with the slide
 					slide.add_department(department.name)
 				else:
 					return {"message": f"Department [{department_name}] not found"}, 404
-	
+
 		if 'content' in args and args['content']:
 			slide.content = args['content']
 
@@ -100,10 +91,9 @@ class SlideResource(Resource):
 
 		slide.save(self.mongo)
 
-		return {'message': 'Slide updated', 'slide_title': slide_title}, 200	
+		return {'message': 'Slide updated', 'slide_title': slide_title}, 200
 
-		
-	
+	@marshal_with(slide_fields)
 	def put(self, slide_title):
 		args = slide_parser.parse_args()
 		title = args['title']
@@ -111,14 +101,11 @@ class SlideResource(Resource):
 		author_id = args['author_id']
 		departments = args.get('departments', [])
 
-		print(departments)
-
 		slide = Slide.find_by_title(slide_title, self.mongo)
 
 		if not slide:
 			return {"message": "Slide not found"}, 404
-		
-		# Update the slide attributes
+
 		slide.title = title
 		slide.content = content
 		slide.author_id = author_id
@@ -129,15 +116,10 @@ class SlideResource(Resource):
 			department = Department.find_by_name(department_name, self.mongo)
 
 			if department:
-				# Associate the department with the slide
 				slide.add_department(department.name)
 			else:
 				return {"message": f"Department [{department_name}] not found"}, 404
 
-		# Save the updated slide
 		slide.save(self.mongo)
-	
-		return {'message': 'Slide updated', 'slide_title': slide_title}, 200
-	
-# Add API routes for adding departments and slides
 
+		return {'message': 'Slide updated', 'slide_title': slide_title}, 200
