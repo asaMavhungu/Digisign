6a81f2869ad8096a53d54036ef8f428cbae3947e
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-09-22 00:12:37 +0200

    upload works

--- digisignAPI/client/views.py
+++ digisignAPI/client/views.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, render_template, request, flash, send_file
+from werkzeug.utils import secure_filename
+import os
 
 views = Blueprint('views', __name__)
+
+
 
 @views.route('/', methods=['GET'])
 def home():
@@ -9,7 +13,7 @@ def home():
 
 @views.route('/devices', methods=['GET'])
 def devices():
-	return send_file("client/html/ten.html")
+	return send_file("client/html/all-devices.html")
 
 @views.route('/signup', methods=['GET'])
 def test():
@@ -30,3 +34,40 @@ def signin():
 @views.route('/deps', methods=['GET'])
 def departments():
 	return send_file("client/html/department.html")
+
+@views.route('/uploadPage', methods=['GET'])
+def upload():
+	return send_file("client/html/uploadpage.html")
+
+@views.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+
+    if uploaded_file.filename != '':
+        # Get the directory of this views.py file
+        current_directory = os.path.dirname(__file__)
+
+        # Navigate to the directory containing main.py
+        parent_directory = os.path.dirname(current_directory)
+
+        # Specify the relative path to the "uploads" directory
+        file_directory = os.path.join(parent_directory, 'uploads')
+
+        # Create the "uploads" directory if it doesn't exist
+        if not os.path.exists(file_directory):
+            os.makedirs(file_directory)
+
+        # Build the absolute path for the uploaded file
+        file_path_relative = os.path.join('uploads', secure_filename(uploaded_file.filename))
+        file_path_absolute = os.path.abspath(os.path.join(file_directory, secure_filename(uploaded_file.filename)))
+
+        print("Absolute path:", file_path_absolute)
+
+        # Save the uploaded file
+        uploaded_file.save(file_path_absolute)
+
+        # Do any additional processing you need here
+
+        return 'File uploaded successfully!'
+    else:
+        return 'No file selected.'
