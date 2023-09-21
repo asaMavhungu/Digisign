cd6d5c552237664ad1841d461d4a99a3e37a0ee4
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-09-22 01:43:35 +0200

    fixed upload

--- digisignAPI/client/views.py
+++ digisignAPI/client/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, send_file
+from flask import Blueprint, render_template, request, flash, send_file, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import os
 
@@ -6,10 +6,19 @@ views = Blueprint('views', __name__)
 
 
 
-@views.route('/', methods=['GET'])
-def home():
+@views.route('/docs', methods=['GET'])
+def docs():
 	return send_file("client/ApiSpecs/redoc-static.html")
 
+
+@views.route('/', methods=['GET'])
+def home():
+	# Assuming 'image.jpg' is the name of your file in the 'uploads' directory
+	return send_file("client/html/home.html")
+
+@views.route('/signin', methods=['GET'])
+def signin():
+	return send_file("client/html/login.html")
 
 @views.route('/devices', methods=['GET'])
 def devices():
@@ -27,9 +36,7 @@ def slides():
 def login():
 	return send_file("client/html/login.html")
 
-@views.route('/signin', methods=['GET'])
-def signin():
-	return send_file("client/html/signin.html")
+
 
 @views.route('/deps', methods=['GET'])
 def departments():
@@ -39,35 +46,40 @@ def departments():
 def upload():
 	return send_file("client/html/uploadpage.html")
 
+
+@views.route('/uploads/<filename>')
+def serve_image(filename):
+    return send_from_directory('uploads', filename)
+
 @views.route('/upload', methods=['POST'])
 def upload_file():
-    uploaded_file = request.files['file']
+	uploaded_file = request.files['file']
 
-    if uploaded_file.filename != '':
-        # Get the directory of this views.py file
-        current_directory = os.path.dirname(__file__)
+	if uploaded_file.filename != '':
+		# Get the directory of this views.py file
+		current_directory = os.path.dirname(__file__)
 
-        # Navigate to the directory containing main.py
-        parent_directory = os.path.dirname(current_directory)
+		# Navigate to the directory containing main.py
+		parent_directory = os.path.dirname(current_directory)
 
-        # Specify the relative path to the "uploads" directory
-        file_directory = os.path.join(parent_directory, 'uploads')
+		# Specify the relative path to the "uploads" directory
+		file_directory = os.path.join(parent_directory, 'uploads')
 
-        # Create the "uploads" directory if it doesn't exist
-        if not os.path.exists(file_directory):
-            os.makedirs(file_directory)
+		# Create the "uploads" directory if it doesn't exist
+		if not os.path.exists(file_directory):
+			os.makedirs(file_directory)
 
-        # Build the absolute path for the uploaded file
-        file_path_relative = os.path.join('uploads', secure_filename(uploaded_file.filename))
-        file_path_absolute = os.path.abspath(os.path.join(file_directory, secure_filename(uploaded_file.filename)))
+		# Build the absolute path for the uploaded file
+		file_path_relative = os.path.join('uploads', secure_filename(uploaded_file.filename))
+		file_path_absolute = os.path.abspath(os.path.join(file_directory, secure_filename(uploaded_file.filename)))
 
-        print("Absolute path:", file_path_absolute)
+		print("Absolute path:", file_path_absolute)
 
-        # Save the uploaded file
-        uploaded_file.save(file_path_absolute)
+		# Save the uploaded file
+		uploaded_file.save(file_path_absolute)
 
-        # Do any additional processing you need here
+		# Do any additional processing you need here
 
-        return 'File uploaded successfully!'
-    else:
-        return 'No file selected.'
+		return 'File uploaded successfully!'
+	else:
+		return 'No file selected.'
