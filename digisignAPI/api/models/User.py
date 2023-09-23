c0b1ee28e298dcbf3479fa5b5506771b1dfbfcd8
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-09-24 01:31:07 +0200

    user GET works

--- digisignAPI/api/models/User.py
+++ digisignAPI/api/models/User.py
@@ -50,5 +50,33 @@ class User:
 	def find_by_name(user_name: str):
 
 		result, code = sql_client.get_entry('users', {'username': user_name,})
-		
+
 		return result, code
+	
+	def create_database_entry(self):
+		result = sql_client.create_entry('users', data = {
+			'username': self.username,  # Replace with your actual data
+			'email': self.email,
+			'password': self.password,
+			# Add other fields as needed
+		} )
+
+		return result
+	
+	def update_database_entry(self, data: dict):
+
+		if 'username' in data:
+			self.department_name = data['username']
+		result = sql_client.update_entry('users', 
+				filter_dict={'user_id': self.user_id},
+				data = data
+			)
+		
+		return result
+	
+	def delete_database_entry(self):
+		result = sql_client.delete_entry('users', 
+				filter_dict={'user_id': self.user_id}
+			)
+		
+		return result
